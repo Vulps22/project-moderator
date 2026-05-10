070f3579bea93afe22b852565a627b799ad71205
Vulps22 <ajmcallister5@gmail.com>
2026-05-10 11:12:18 +0100

    #1 remove msMiddleware from ping route middleware

--- src/routes/api/v1/ping.ts
+++ src/routes/api/v1/ping.ts
@@ -1,8 +1,7 @@
 import { ApiRoute } from '@vulps22/dynamic-endpoint-router';
-import { msMiddleware } from '../../../middleware/msAuth';
 
 export const route: ApiRoute = {
-  middleware: msMiddleware,
+  middleware: [],
   async get(_req, res): Promise<void> {
     res.status(200).json({ message: 'OK' });
   },
